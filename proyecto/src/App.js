2c31298f88f8eb3b2658be99b9b3fc2cd0a8f3f5
andresxy120 <58017027+andresxy120@users.noreply.github.com>
2023-10-09 22:34:19 -0500

    Create App.js

---
+++ proyecto/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './partpagina/Header'
+import Inicio from './partpagina/Inicio';
+function App() {
+  return (
+    <section className="App">
+      <Header/>
+      <Inicio/>
+    </section>
+  );
+}
+
+export default App;
