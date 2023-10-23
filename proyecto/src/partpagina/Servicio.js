f6f0d38e13fb001b68e1384ad13fa35f05750f71
andresxy120 <58017027+andresxy120@users.noreply.github.com>
2023-10-22 21:32:56 -0500

    Update Servicio.js

--- proyecto/src/partpagina/Servicio.js
+++ proyecto/src/partpagina/Servicio.js
@@ -5,34 +5,59 @@ const Servicio =()=>{
     return(
         <Contenedor className="principal">
             <Contenedor>
-                <Titulo>
-                Servicio complejo para su automóvil
+                <Lista>
+                    <li>
+                    <Titulo>
+                    Servicio complejo para su motocicleta
                    
-                </Titulo>
-                <Parrafo>
-                Nuestros asesores de automóviles de fácil contacto le <br/>
-                explicarán cualquier mantenimiento o reparación <br/> 
-                sugeridos. No tomarán ninguna medida a menos que las <br/>
-                apruebe primero. Nuestros técnicos de motocicleta<br/>
-                certificados por ASE están cuidando su motocicleta y su <br/>
-                presupuesto.
-                </Parrafo>
+                    </Titulo>
+                    <Parrafo>
+                    Nuestros asesores de motocicleta de fácil contacto le <br/>
+                    explicarán cualquier mantenimiento o reparación <br/> 
+                    sugeridos. No tomarán ninguna medida a menos que las <br/>
+                    apruebe primero. Nuestros técnicos de motocicleta<br/>
+                    certificados por ASE están cuidando su motocicleta y su <br/>
+                    presupuesto.
+                    </Parrafo>
+                    </li>
+                    <li>
+                        <Imagenes/>
+                    </li>
+                </Lista>
+               
                 <Titulo>
-                <center>Una lista de verificación para preparar su automóvil para un largo viaje por carretera</center>
+                <center>Una lista de verificación para preparar su motocicleta para un largo viaje por carretera</center>
                 </Titulo>
                 <Lista>
                     <li>
                         <Imagenes/>
                         <Subtitulo>
+                        Mantenimiento general
                         </Subtitulo>
                         <Parrafo>
+                        -Verifica el manual del propietario de tu motocicleta para conocer los intervalos de <br/>
+                        mantenimiento recomendados y asegúrate de que todo esté al día.
+                        -Realiza un cambio de aceite y filtros si es necesario.
+                        -Asegúrate de que los frenos estén en buen estado y ajusta las pastillas <br/>
+                         o tambores si es necesario.
+                        -Comprueba el estado de la cadena, la tensión y la lubricación.
+                        -Revisa el sistema de escape en busca de fugas o daños.
+                        -Asegúrate de que los neumáticos estén en buen estado y con la presión <br/>
+                         adecuada. Lleva una llanta de repuesto y las herramientas necesarias para cambiarla si <br/>
+                         es posible.
                         </Parrafo>
                     </li>
                     <li>
                         <Imagenes/>
                         <Subtitulo>
+                            Seguridad y visibilidad
                         </Subtitulo>
                         <Parrafo>
+                        -Asegúrate de que todas las luces, incluidos los faros, las luces <br/>
+                         de freno y las luces direccionales, funcionen correctamente.
+                        -Limpia y ajusta los espejos retrovisores.
+                        -Asegúrate de tener un chaleco reflectante y elementos reflectantes en tu equipo.
+                        -Lleva un kit de herramientas básico, una linterna y una navaja multiusos.
                         </Parrafo>
                     </li>
                 </Lista>
@@ -42,10 +67,12 @@ const Servicio =()=>{
                 ¿Por qué elegir nuestro servicio?
                 </Titulo>
                 <Parrafo>
-                La principal ventaja de la cooperación con el centro de reparación de automóviles es la ventajosa relación calidad-precio.
+                La principal ventaja de la cooperación con el centro<br/>
+                de reparación de motocicleta es la ventajosa relación<br/>
+                calidad-precio.
                 </Parrafo>
                 <Titulo>
-                Restauración de coches de alta calidad
+                Reparacion de motocicleta de alta calidad
                 </Titulo>
                 <Lista>
                     <li>
@@ -53,8 +80,12 @@ const Servicio =()=>{
                     </li>
                     <li>
                         <Subtitulo>
+                        <center>Reparación</center> 
                         </Subtitulo>
                         <Parrafo>
+                         <center>La reparación de una motocicleta es un proceso<br/>
+                          fundamental para mantener su seguridad, rendimiento y <br/>
+                          durabilidad a lo largo del tiempo.</center>
                         </Parrafo>
                     </li>
                     <li>
@@ -62,8 +93,13 @@ const Servicio =()=>{
                     </li>
                     <li>
                         <Subtitulo>
+                        <center>Cuidado de la motocicleta</center>
                         </Subtitulo>
                         <Parrafo>
+                        <center>El cuidado adecuado de la motocicleta es esencial<br/>
+                        para garantizar su durabilidad, rendimiento y seguridad,<br/>
+                         y para prevenir problemas mecánicos, eléctricos o de<br/>
+                        otro tipo</center>
                         </Parrafo>
                     </li>
                     <li>
@@ -71,8 +107,11 @@ const Servicio =()=>{
                     </li>
                     <li>
                         <Subtitulo>
+                        <center>Calidad</center>
                         </Subtitulo>
                         <Parrafo>
+                        <center>nivel de rendimiento, grado de perfección o cumplimiento<br/>
+                        de estándares específicos que se aplican a un servicio.</center>
                         </Parrafo>
                     </li>
                 </Lista>
@@ -102,10 +141,12 @@ const Servicio =()=>{
             </Contenedor>
             <Contenedor>
                 <Titulo>
-                Lo que dicen los clientes
+                <center>Lo que dicen los clientes</center>
                 </Titulo>
                 <Parrafo>
-                Valoramos enormemente las relaciones sólidas y hemos visto los beneficios que aportan a nuestro negocio. Los comentarios de los clientes son vitales para ayudarnos a hacerlo bien.
+                <center>Valoramos enormemente las relaciones sólidas y hemos visto los beneficios que<br/>
+                aportan a nuestro negocio. Los comentarios de los clientes son vitales para<br/>
+                ayudarnos a hacerlo bien.</center>
                 </Parrafo>
                 <Lista>
                     <li>
