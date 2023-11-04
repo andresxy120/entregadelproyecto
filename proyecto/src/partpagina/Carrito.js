e3817be9f3408ccdaeedaa1c6442f976ac184f2d
andresxy120 <58017027+andresxy120@users.noreply.github.com>
2023-11-04 12:13:28 -0500

    Create Carrito.js

---
+++ proyecto/src/partpagina/Carrito.js
@@ -0,0 +1,28 @@
+import React from "react";
+import stilos from "styled-components";
+
+const Carrito =()=>{
+    return(
+        <Contenedor>
+            <Titulo>Carrito</Titulo>
+            <Parrafo>Tu carrito esta vacio</Parrafo>
+        </Contenedor>
+    )
+
+};
+export default Carrito;
+const Contenedor=stilos.section`
+`
+const Titulo=stilos.h1`
+`
+const Parrafo=stilos.p`
+`
+const Lista=stilos.ul`
+`
+const Imagenes=stilos.img`
+`
+const Subtitulo=stilos.h2`
+`
+const Boton=stilos.button`
+`
+const 
