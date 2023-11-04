7c3ac825632f675f73f09c7f5a716f4c172481ef
andresxy120 <58017027+andresxy120@users.noreply.github.com>
2023-11-03 22:39:56 -0500

    Update Productos.js

---
+++ proyecto/src/partpagina/Productos.js
@@ -0,0 +1,52 @@
+import React from "react";
+import stilos from "styled-components";
+import producto1 from './imgproducto/producto1.jpg'
+import producto2 from './imgproducto/producto2.jpg'
+const Productos=()=>{
+    const productos = [
+        {
+          id: 1,
+          nombre: "Producto 1",
+          precio: 10.99,
+          imagen: producto1,
+        },
+        {
+          id: 2,
+          nombre: "Producto 2",
+          precio: 19.99,
+          imagen: producto2,
+        },
+        // Agrega más productos aquí
+      ];
+    return(
+      <Contenedor>
+      <Titulo>Productos Disponibles</Titulo>
+      <Contenedor className="productos-list">
+        {productos.map((producto) => (
+          <Contenedor className="producto" key={producto.id}>
+            <Imagenes src={producto.imagen} alt={producto.nombre} />
+            <Subtitulo>{producto.nombre}</Subtitulo>
+            <Parrafo>Precio: ${producto.precio.toFixed(2)}</Parrafo>
+            <Boton>Agregar al Carrito</Boton>
+          </Contenedor>
+        ))}
+      </Contenedor>
+    </Contenedor>
+    )
+}
+export default Productos;
+
+const Contenedor=stilos.section`
+`
+const Titulo=stilos.h1`
+`
+const Parrafo=stilos.p`
+`
+const Lista=stilos.ul`
+`
+const Imagenes=stilos.img`
+`
+const Subtitulo=stilos.h2`
+`
+const Boton=stilos.button`
+`
