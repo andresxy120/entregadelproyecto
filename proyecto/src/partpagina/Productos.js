7ea04a3dfe52f286f07ed163587b1b95c83627ac
andresxy120 <58017027+andresxy120@users.noreply.github.com>
2023-11-13 22:06:54 -0500

    Update Productos.js

--- proyecto/src/partpagina/Productos.js
+++ proyecto/src/partpagina/Productos.js
@@ -2,46 +2,50 @@ import React from "react";
 import stilos from "styled-components";
 import producto1 from './imgproducto/producto1.jpg'
 import producto2 from './imgproducto/producto2.jpg'
-const Productos=()=>{
-    const productos = [
-        {
-          titulo: "Producto 1",
-          precio: 65.000,
-          imagen: producto1,
-          categorias:{
-            id:"Motor",
-            nombre:"Motor"
-          }
-        },
-        {
-          titulo: "Producto 2",
-          precio: 80.000,
-          imagen: producto2,
-          categorias:{
-            id:"Motor",
-            nombre:"Motor"
-          }
-        },
-        // Agrega más productos aquí
-      ];
-    return(
+import { Link, Outlet } from "react-router-dom";
+
+const producid = [
+  {
+    titulo: "Producto 1",
+    precio: 65.000,
+    imagen: producto1,
+    categorias: {
+      id: "Motor",
+      nombre: "Motor"
+    }
+  },
+  {
+    titulo: "Producto 2",
+    precio: 80.000,
+    imagen: producto2,
+    categorias: {
+      id: "Motor",
+      nombre: "Motor"
+    }
+  },
+  // Agrega más productos aquí
+];
+
+const Productos = () => {
+  return (
+    <Contenedor>
       <Contenedor>
-        <Contenedor>
         <nav>
-                <Lista>
-                    <li><button class="">Todos los productos</button></li>
-                    <li><button>Motor</button></li>
-                    <li><button>Luces</button></li>
-                    <li><button>Accesorios</button></li>
-                    <li><button>Exterior</button></li>
-                    <li><button>Diseño</button></li>
-                    <li><a href="#"></a><span class="numerito">0</span></li>
-                </Lista>
-            </nav>
-        </Contenedor>
+          <Lista>
+            <li><Boton className="">Todos los productos</Boton></li>
+            <li><Boton id="motor">Motor</Boton></li>
+            <li><Boton id="luces">Luces</Boton></li>
+            <li><Boton id="accesorios">Accesorios</Boton></li>
+            <li><Boton id="exterior">Exterior</Boton></li>
+            <li><Boton id="diseño">Diseño</Boton></li>
+            <li><Link to="/Carrito">carrito</Link></li>
+          </Lista>
+        </nav>
+        <Outlet/>
+      </Contenedor>
       <Titulo>Productos Disponibles</Titulo>
       <Contenedor className="productos-list">
-        {productos.map((producto) => (
+        {producid.map((producto) => (
           <Contenedor className="producto" key={producto.categorias.id}>
             <Imagenes src={producto.imagen} alt={producto.categorias.nombre} />
             <Subtitulo>{producto.titulo}</Subtitulo>
@@ -51,21 +55,23 @@ const Productos=()=>{
         ))}
       </Contenedor>
     </Contenedor>
-    )
+  );
 }
+
+export { producid };
 export default Productos;
 
-const Contenedor=stilos.section`
+const Contenedor = stilos.section`
 `
-const Titulo=stilos.h1`
+const Titulo = stilos.h1`
 `
-const Parrafo=stilos.p`
+const Parrafo = stilos.p`
 `
-const Lista=stilos.ul`
+const Lista = stilos.ul`
 `
-const Imagenes=stilos.img`
+const Imagenes = stilos.img`
 `
-const Subtitulo=stilos.h2`
+const Subtitulo = stilos.h2`
 `
-const Boton=stilos.button`
+const Boton = stilos.button`
 `
