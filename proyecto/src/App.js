4e49e146e3a5e4d9c9d67f61bf0ef5a88392beb8
andresxy120 <58017027+andresxy120@users.noreply.github.com>
2023-11-13 22:07:04 -0500

    Update App.js

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -1,13 +1,23 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Productos from './partpagina/Productos';
-//import Header from './partpagina/Header'
-//import Inicio from './partpagina/Inicio';
+import Carrito from './partpagina/Carrito';
+import Header from './partpagina/Header'
+import Inicio from './partpagina/Inicio';
 import Servicio from './partpagina/Servicio';
 function App() {
   return (
     <section className="App">
-      <Productos/>
+      <Routes>
+          <Route path='/' element={<Header/>}>
+              <Route path='/' element={<Inicio/>}></Route>
+              <Route path='Productos' element={<Productos/>}></Route>
+              <Route path='Servicio' element={<Servicio/>}></Route>
+              <Route path='Carrito' element={<Carrito/>}></Route>
+              <Route path='*' element={<Inicio/>}></Route>
+          </Route>
+      </Routes>
     </section>
   );
 }
