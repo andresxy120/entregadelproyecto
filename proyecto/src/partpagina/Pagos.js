dd679ec0079fce7e9366a4edf40303d9c857b45b
andresxy120 <achaverrahiguita@gmail.com>
2023-11-21 01:28:42 -0500

    Update Pagos.js

--- proyecto/src/partpagina/Pagos.js
+++ proyecto/src/partpagina/Pagos.js
@@ -6,38 +6,38 @@ const Pagos=()=>{
         <Contenedor>
              <form>
                 <Lista>
-                    <li>
+                    <Li>
                         <label>Nombre de la Tarjeta</label>
-                        <input type="text" id="nombre-tarjeta"/>
-                    </li>
-                    <li>
+                        <Input type="text" id="nombre-tarjeta"/>
+                    </Li>
+                    <Li>
                         <label>Tipos de Documento</label>
                         <select>
                             <option>C.C</option>
                             <option>C.E</option>
                             <option>CC DIG</option>
                         </select>
-                    </li>
-                    <li>
+                    </Li>
+                    <Li>
                         <label>Numero de la Tarjeta</label>
-                        <input type="text" id="numero-tarjeta"/>
-                    </li>
-                    <li>
+                        <Input type="text" id="numero-tarjeta"/>
+                    </Li>
+                    <Li>
                         <label>Codigo de Seguridad</label>
-                        <input type="text" id="codigo"/>
-                    </li>
-                    <li>
+                        <Input type="text" id="codigo"/>
+                    </Li>
+                    <Li>
                         <label>Fecha de Vencimiento</label>
-                        <input type="text" id="mes"/>
-                        <input type="text" id="año"/>
-                    </li>
-                    <li>
+                        <Input type="text" id="mes" placeholder="MM"/>
+                        <Input type="text" id="año" placeholder="AA"/>
+                    </Li>
+                    <Li>
                         <label>Telefono celular</label>
-                        <input type="text" id="telefono"/>
-                    </li>
-                    <li>
-                        <button type="submit">Pagar</button>
-                    </li>
+                        <Input type="text" id="telefono"/>
+                    </Li>
+                    <Li>
+                        <Boton type="submit">Pagar</Boton>
+                    </Li>
                 </Lista>
              </form>
         </Contenedor>
@@ -46,6 +46,33 @@ const Pagos=()=>{
 
 export default Pagos;
 const Contenedor=stylos.section`
+display: flex;
+justify-content: center;
 `
 const Lista=stylos.ul`
+    list-style-type: none;
+`
+const Input = stylos.input`
+    display: flex;
+  padding: 8px 60px;
+  margin: 0 20px;
+  justify-content: flex-end;
+
+  
+`
+const Boton = stylos.button`
+  width: 100px;
+  height: 50px; 
+  padding: 0; 
+  text-align: center;
+  background-color: #fc8600;
+  border: none;
+  font-weight: bold;
+`
+const Li=stylos.li`
+display: flex;
+justify-content: flex-start;
+align-items: 
+margin:10px;
+
 `
